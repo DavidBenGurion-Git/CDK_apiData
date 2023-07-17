4a267a717758e738c2dda8b30edb62083a11b6f2
David Ben Gurion <davidtj5222@gmail.com>
2023-07-17 16:50:49 +1000

    added dictionary lookup

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -8,22 +8,46 @@ def handler(event, context):
     bucket_name = os.environ['BUCKET_NAME']
     api_endpoint = os.environ['API_ENDPOINT']
 
-    latitudes = [51.5098, 48.8588897, 50.8465573, 40.4167047, 47.48138955, 59.97239745]
-    longitudes = [-0.1180, 2.3200410217200766, 4.351697, -3.7035825, 19.14609412691246, 10.775729194051895]
-    locations = ["London", "Paris", "Brussels", "Madrid", "Budapest", "Oslo"]
+    location_data = {
+        'London': {
+            'latitude': 51.5098,
+            'longitude': -0.1180
+        },
+        'Paris': {
+            'latitude': 48.8588897,
+            'longitude': 2.3200410217200766
+        },
+        'Brussels': {
+            'latitude': 50.8465573,
+            'longitude': 4.351697
+        },
+        'Madrid': {
+            'latitude': 40.4167047,
+            'longitude': -3.7035825
+        },
+        'Budapest': {
+            'latitude': 47.48138955,
+            'longitude': 19.14609412691246
+        },
+        'Oslo': {
+            'latitude': 59.97239745,
+            'longitude': 10.775729194051895
+        }
+    }
+
     current_timestamp = int(datetime.now().timestamp())
 
     response_data = []
-    for i in range(len(latitudes)):
-        lat = latitudes[i]
-        lon = longitudes[i]
+    for location, coordinates in location_data.items():
+        lat = coordinates['latitude']
+        lon = coordinates['longitude']
         endpoint = api_endpoint.format(lat=lat, lon=lon).replace('end=dynamic', f'end={current_timestamp}')
 
         response = requests.get(endpoint)
         if response.status_code == 200:
             data = response.json()
             for item in data["list"]:
-                item["location"] = locations[i]
+                item["location"] = location
             response_data.append(data)
         else:
             return {
@@ -33,7 +57,8 @@ def handler(event, context):
 
     merged_data = {
         'coord': response_data[0]['coord'],
-        'list': [item for data in response_data for item in data['list']]
+        'list': [item for data in response_data for item in data['list']],
+        'locations': location_data
     }
 
     json_data = json.dumps(merged_data)
